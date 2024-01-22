c10d83cb964b498081e0f6b2e2e76589c46114a7
UJIN901 <dalvit9655@gmail.com>
2024-01-23 00:51:13 +0900

    init

---
+++ src/main/java/com/ll/testtt/TestttApplication.java
@@ -0,0 +1,13 @@
+package com.ll.testtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestttApplication.class, args);
+    }
+
+}
